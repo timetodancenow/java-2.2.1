39ec538530e1f7a0691fb0d7905e04cd8d767898
Карина Аллахвердиева <timetodancenow@yandex.ru>
2022-05-11 16:54:09 +0300

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main ( String[] args ) {
+        int price = 1450;
+        int amount = 20;
+        int bonus;
+        bonus = price / amount;
+        System.out.println ( " Бонус: " + bonus);
+    }
+}
